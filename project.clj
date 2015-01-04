eaeae4e14672865330d2ba777d6f8cd609312934
Paul Sabda <sabdaps@gmail.com>
2015-01-04 07:57:55 +0700

    commit

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.datomic/datomic-pro "0.9.5078"]])
+                 [com.datomic/datomic-pro "0.9.5078" :exclusions [joda-time]]
+                 [couchbase-clj "0.2.0"]])
--- src/datalfa/core.clj
+++ src/datalfa/core.clj
@@ -1,6 +1,6 @@
 (ns datalfa.core
   (:require [datomic.api :as d]))
 
-(def uri "datomic:couchbase://localhost/alfa/mistra")
+(def uri "datomic:couchbase://localhost:4334/alfa/mistra")
 
 
